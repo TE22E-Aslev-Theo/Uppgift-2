d9842aa7982d9fb5843a2faa9aed5334b9d453c6
TE22E-Aslev-Theo <theo.aslev@elev.ga.ntig.se>
2023-09-18 09:12:41 +0200

    Uppgift 2

---
+++ Uppgift2.java
@@ -0,0 +1,19 @@
+
+
+public class Uppgift2{
+
+    public static void main(String[] args) {
+        
+        int tal1 = 2;
+        int tal2 = 30;
+
+        System.out.println("Summan av tal 1 + tal 2 är " + (tal1 + tal2)); 
+        //Jag Improviserade koden lite med att Inte göra För många variablar
+        System.out.println("Summan av tal 1 - tal 2 är " + (tal1 - tal2));
+
+        System.out.println("Produkten av tal 1 och tal 2 är " + (tal1 * tal2));
+
+        System.out.println("Kvoten av tal 1 och tal 2 är " + (tal1 / tal2));
+
+    }
+}
